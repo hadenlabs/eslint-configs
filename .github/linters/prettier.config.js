026aae4bf662d4749ed236625019844694265823
@slovacus <slovacus@gmail.com>
2021-04-21 11:21:58 -0500

    refactor: configuration prettier standard (#2)

--- .github/linters/prettier.config.js
+++ .github/linters/prettier.config.js
@@ -10,49 +10,55 @@ module.exports = {
     {
       files: '{*.js?(on),*.y?(a)ml,.*.js?(on),.*.y?(a)ml,*.md,.prettierrc,.stylelintrc,.babelrc}',
       options: {
+        arrowParens: 'always',
+        printWidth: 120,
+        semi: false,
+        singleQuote: true,
+        quoteProps: 'as-needed',
         tabWidth: 2,
-      },
+        trailingComma: 'none'
+      }
     },
     {
-      files: '{**/.vscode/*.json,**/tsconfig.json,**/tsconfig.*.json,.stylelintrc}',
+      files: '{**/.vscode/*.json,**/tsconfig.json,**/tsconfig.*.json}',
       options: {
         parser: 'json',
         quoteProps: 'preserve',
-        singleQuote: false,
-      },
+        singleQuote: false
+      }
     },
     {
       files: '*.md',
       options: {
         parser: 'markdown',
-        printWidth: 100,
+        printWidth: 120,
         proseWrap: 'never',
         semi: false,
-        trailingComma: 'none',
-      },
+        trailingComma: 'none'
+      }
     },
     {
       files: '*.mdx',
       options: {
-        printWidth: 100,
+        printWidth: 120,
         proseWrap: 'never',
         semi: false,
-        trailingComma: 'none',
-      },
+        trailingComma: 'none'
+      }
     },
     {
       files: '*.{sass,scss}',
       options: {
-        parser: 'scss',
-      },
+        parser: 'scss'
+      }
     },
     {
       files: '*.less',
       options: {
         tabWidth: 4,
         printWidth: 80,
-        parser: 'less',
-      },
-    },
-  ],
+        parser: 'less'
+      }
+    }
+  ]
 }
